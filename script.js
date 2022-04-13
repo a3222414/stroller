6654412c91de2e048abaf524d95ec4999d2438cb
Erik Chen <ericisjustinbieber@gmail.com>
2022-04-14 02:11:11 +0800

    加入跟隨menuBar 跟 menuBar滑鼠進，其他透明

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ const topBarBtn = document.querySelector('.top-reccommendation-btn');
 const topBarBox = document.querySelector('.top-reccommendation-box');
 //Header
 const header = document.getElementsByTagName('header');
+const header1 = document.querySelector('header');
 const headerButton = document.querySelector('.header-button');
-
+///Feature container
+const featureContainer = document.querySelector('.feature-container');
 //Buy box
 const buyBox = document.querySelector('.buy-box');
 //   TOP BAR!!!!!!
@@ -22,8 +24,8 @@ topBarBtn.addEventListener('click', function () {
 
 const headerButtonDimension = headerButton.getBoundingClientRect();
 const buyBoxDimension = buyBox.getBoundingClientRect();
-console.log(buyBoxDimension);
-console.log(pageXOffset, pageYOffset);
+// console.log(buyBoxDimension);
+// console.log(pageXOffset, pageYOffset);
 
 headerButton.addEventListener('click', function () {
   window.scrollTo({
@@ -44,7 +46,54 @@ topMenuBox.addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+////////
+/////    TOP MENU BAR STICKY
+// const featureContainerTop = featureContainer.getBoundingClientRect().top;
+// console.log(featureContainerTop);
+const headerHeight = header1.getBoundingClientRect().height;
+// console.log(headerHeight);
+const obsCallback = function (entries) {
+  const [object] = entries;
+  // console.log(object);
+  object.isIntersecting === false
+    ? topMenuBox.classList.add('sticky')
+    : topMenuBox.classList.remove('sticky');
+};
+const obsOptions = {
+  root: null,
+  rootMargin: `${headerHeight}px`,
+  threshold: 1,
+};
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(header1);
 
+//////// TOP MENU OPACITY    /////////
+topMenuBox.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('menu-link')) {
+    const link = e.target;
+    const linkOl = e.target.parentElement;
+    // console.log(linkOl);
+    // console.log(link);
+    const menuLinkElements = link.closest('ul').querySelectorAll('ol');
+    // console.log(menuLinkElements);
+    menuLinkElements.forEach((el) => {
+      if (el !== linkOl) el.style.opacity = 0.5;
+    });
+  }
+});
+topMenuBox.addEventListener('mouseout', function (e) {
+  if (e.target.classList.contains('menu-link')) {
+    const link = e.target;
+    const linkOl = e.target.parentElement;
+    // console.log(linkOl);
+    // console.log(link);
+    const menuLinkElements = link.closest('ul').querySelectorAll('ol');
+    // console.log(menuLinkElements);
+    menuLinkElements.forEach((el) => {
+      el.style.opacity = 1;
+    });
+  }
+});
 //Testimonial change people
 const testimonial = document.querySelector('.testimonial');
 const testimonialIcon = document.querySelector('.testimonial-change-icon');
